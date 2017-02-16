7fc03dbca8bebb114352b393f859eea3aab68a4a
Julia Mays <jm27746@gmail.com>
2017-02-16 17:31:17 +0000

    finished for today

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'doses/new'
+
+  get 'doses/create'
+
+  get 'doses/destroy'
+
+  resources :cocktails do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :doses
+end
 end
